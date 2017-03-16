7227012cd96f6601cc1bde2c87d294d6c6b876ec
Toni Fang <tonifang@189.cn>
2017-03-16 13:00:37 +0800

    1st initial -- add wikitude sdk & zbar sdk

---
+++ ScanCode/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  ScanCode
+//
+//  Created by Toni Fang on 2017/3/16.
+//  Copyright © 2017年 Toni Fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
