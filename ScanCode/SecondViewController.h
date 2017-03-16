f127212783df79c7de837715ed3eeb5e190a5ce4
Toni Fang <tonifang@189.cn>
2017-03-16 10:29:26 +0800

    Initial Commit

---
+++ ScanCode/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  ScanCode
+//
+//  Created by Toni Fang on 2017/3/16.
+//  Copyright © 2017年 Toni Fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
